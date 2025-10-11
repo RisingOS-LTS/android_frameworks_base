fab843797250e90401c0bc7a9c007fcbe1877805
ayushqui <proayush736@gmail.com>
2025-10-11 18:28:46 +0000

    name
    Change-Id: Icddba6e17188dbfd6d2dc93d9ed97774012776e6

--- core/java/com/android/internal/util/rising/SystemRestartUtils.java
+++ core/java/com/android/internal/util/rising/SystemRestartUtils.java
@@ -14,7 +14,7 @@
  * limitations under the License.
  */
 
-package com.android.internal.util.crdroid;
+package com.android.internal.util.rising;
 
 import android.app.AlertDialog;
 import android.app.IActivityManager;
