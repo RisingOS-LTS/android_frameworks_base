11f3aa44dded9434fb54809ef656656b5bcee8ac
ayushqui <proayush736@gmail.com>
2025-10-16 16:04:25 +0000

    fix
    Change-Id: I36073f134e7720e7cada62e9b26476a92ccc9587

--- packages/SystemUI/src/com/android/systemui/qs/PagedTileLayout.java
+++ packages/SystemUI/src/com/android/systemui/qs/PagedTileLayout.java
@@ -7,6 +7,7 @@ import android.animation.AnimatorListenerAdapter;
 import android.animation.AnimatorSet;
 import android.animation.ObjectAnimator;
 import android.animation.PropertyValuesHolder;
+import android.app.ActivityManager;
 import android.content.Context;
 import android.content.res.Configuration;
 import android.os.Bundle;
